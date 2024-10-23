9dad0570d29e28a70c980af446d3174daa381ea0
DuongHoaiAn2k1 <dhan29112001@gmail.com>
2024-10-23 13:12:50 +0700

    Update new code to local

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <AppHeader />
+  <AppHeader v-if="showHeaderFooter" />
   <div id="layoutSidenav">
-    <SideBar />
+    <SideBar v-if="showHeaderFooter" />
     <router-view />
   </div>
-  <AppFooter />
+  <AppFooter v-if="showHeaderFooter" />
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import AppHeader from "@/components/AppHeader.vue";
 import AppFooter from "@/components/AppFooter.vue";
 import SideBar from "@/components/SideBar.vue";
 
+const route = useRoute();
+
+const showHeaderFooter = computed(() => {
+  return route.name !== 'login';
+});
 </script>
+
 <style>
 body {
   margin: 0;
